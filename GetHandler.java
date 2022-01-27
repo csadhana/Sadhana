9b42a0fc6d6a19e7fa0e9173233556009e7f2b27
csadhana <97276140+csadhana@users.noreply.github.com>
2022-01-27 14:11:36 +0530

    Add files via upload

--- GetHandler.java
+++ GetHandler.java
@@ -1,5 +1,5 @@
-
-import java.io.*;
+package com.sadhana.training;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -7,41 +7,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
-* Servlet implementation class GetHandler
-*/
+ * Servlet implementation class GetHandler
+ */
 @WebServlet("/GetHandler")
 public class GetHandler extends HttpServlet {
-        private static final long serialVersionUID = 1L;
-       
-    /**
-* @see HttpServlet#HttpServlet()
-*/
-    public GetHandler() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.print("<hr>");
+        out.print("<h3>Welcome to Get Handler</h3>");
+		out.print("<hr>");
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        out.println("Enterd Name is=" + name + "<br> Address is =" + address);
+        out.println("</body></html>");
+		out.print("<hr>");
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                  PrintWriter out = response.getWriter();
-                    out.println("<html><body>");
-                    
-                    String name = request.getParameter("name");
-                    String address = request.getParameter("address");
-                    out.println("Name=" + name + "<br>Address=" + address);
-                    out.println("</body></html>");
-        }
-
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                doGet(request, response);
-        }
+	}
+	
 
 }
